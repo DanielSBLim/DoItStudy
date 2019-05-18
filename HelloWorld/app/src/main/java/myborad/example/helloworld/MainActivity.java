c045ff5ac761224158ba1a373752e1c0f935a550
DanielSBLim <lastwin0725@gmail.com>
2019-05-18 14:12:26 +0900

    19-05-18 22:56 Naver site 접속 추가

--- HelloWorld/app/src/main/java/myborad/example/helloworld/MainActivity.java
+++ HelloWorld/app/src/main/java/myborad/example/helloworld/MainActivity.java
@@ -1,5 +1,7 @@
 package myborad.example.helloworld;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -22,11 +24,22 @@ public class MainActivity extends AppCompatActivity {
 
             //선언한 내역을 원래 내역에 대입한다.
             @Override
-            public void onClick(View v) {
+            public void onClick (View v) {
 
                 // 팝업을 만들게 하고 text "시작버튼이 눌렸어오"를 출력한다.
                 Toast.makeText(getApplicationContext(), "시작버튼이 눌렸어요.",
                         Toast.LENGTH_LONG).show();
+            }
+        });
+
+        Button start02Btn = findViewById(R.id.start02Btn);
+        start02Btn.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public  void onClick (View v) {
+                Intent myIntent = new Intent(Intent.ACTION_VIEW,
+                        Uri.parse("http://m.naver.com"));
+                startActivity(myIntent);
             }
         });
     }
