95591014178e924b9efc42af68f880b31ff3bd55
DanielSBLim <lastwin0725@gmail.com>
2019-05-18 11:51:32 +0900

    19-05-18  20:45 1. Button Content "안녕 안드로이드!  → 시작" change 2. Button Click 이후에 팝업 "시작버튼이 눌렀어요" 출력

--- HelloWorld/app/src/main/java/myborad/example/helloworld/MainActivity.java
+++ HelloWorld/app/src/main/java/myborad/example/helloworld/MainActivity.java
@@ -2,6 +2,10 @@ package myborad.example.helloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Button type의 startBtn를 R.id.startBtn를 찾아 선언한다.
+        Button startBtn = findViewById(R.id.startBtn);
+
+        //Button를 클릭하는 메소드를 선언한다.
+        startBtn.setOnClickListener(new View.OnClickListener() {
+
+            //선언한 내역을 원래 내역에 대입한다.
+            @Override
+            public void onClick(View v) {
+
+                // 팝업을 만들게 하고 text "시작버튼이 눌렸어오"를 출력한다.
+                Toast.makeText(getApplicationContext(), "시작버튼이 눌렸어요.",
+                        Toast.LENGTH_LONG).show();
+            }
+        });
     }
+
 }
